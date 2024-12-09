559c78e2a31f00b040e082895e5175e723fcdb5b
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-12-09 15:55:32 -0300

    legal

--- Piece.java
+++ Piece.java
@@ -60,34 +60,36 @@ public class Piece
      * @return Returns a string representation of this given piece
      */
     //@ ensures \result.length()==2;
-    //@ ensures (this.isWhite && this_isKing) ==> (\result.equals("WK"));
-    //@ ensures (this.isWhite && !this_isKing) ==> (\result.equals("W "));
-    //@ ensures (!this.isWhite && this_isKing) ==> (\result.equals("BK"));
-    //@ ensures (!this.isWhite && !this_isKing) ==> (\result.equals("B "));
+    //@ ensures (this.isWhite && this_isKing) <==> (\result.equals("WK"));
+    //@ ensures (this.isWhite && !this_isKing) <==> (\result.equals("W "));
+    //@ ensures (!this.isWhite && this_isKing) <==> (\result.equals("BK"));
+    //@ ensures (!this.isWhite && !this_isKing) <==> (\result.equals("B "));
     //@ pure
     public String getString()
     {
-        String baseSymbol;
+        String baseSymbol = "";
 
-        if (isWhite)
-            baseSymbol = "W";
-        else
-            baseSymbol = "B";
-
-        //@ assert baseSymbol.length()==1;
-        if (isKing)
-            baseSymbol += "K";
-        else
-            baseSymbol += " "; // add a space in the non-king state just to keep consistency
-
+        if (isWhite){
+            if(isKing)
+                baseSymbol = "WK";
+            else
+                baseSymbol = "W ";
+            }
+        else{
+            if(isKing){
+                baseSymbol = "BK";
+            }
+            else
+                baseSymbol = "B ";
+        }
         return baseSymbol;
     }
 
     /**
      * Switches this piece to a king (TODO: MAY BE UNNECCESARY DUE TO BELOW METHOD!!)
      */
-    //@ assignable this.isKing;
-    //@ ensures !this.isKing;
+    //@ assignable this_isKing;
+    //@ ensures this_isKing == true;
     private void setKing()
     {
         this.isKing = true;
@@ -97,21 +99,22 @@ public class Piece
      * Switches this peice to be a king if it is at the end of the board.
      * Should be called after every move.
      */
-    public void checkIfShouldBeKing(Board board)
-    {
-        // if the piece is white, it's a king if it's at the +y, otherwise if its black this happens at the -y side
-        if (isWhite && this.y == board.size - 1 || 
-            !isWhite && this.y == 0)
-            this.setKing();
-    }
+    // public void checkIfShouldBeKing(Board board)
+    // {
+    //     // if the piece is white, it's a king if it's at the +y, otherwise if its black this happens at the -y side
+    //     if (isWhite && this.y == board.size - 1 || 
+    //         !isWhite && this.y == 0)
+    //         this.setKing();
+    // }
 
     /**
      * Moves this piece's reference of its position (DOES NOT ACTUALLY MOVE ON BOARD)
      * @param x The x coordinate of the move
      * @param y The y coordinate of the move
      */
-    // @ assignable this.x, this.y;
-    // @ ensures this.x==1;
+    //@ assignable this_x, this_y;
+    //@ ensures this_x==x;
+    //@ ensures this_y==y;
     public void moveTo(int x, int y)
     {
         this.x = x;
@@ -124,159 +127,159 @@ public class Piece
      * @return Returns a list of all the moves (including recusively found jumps), including each individual one involved in every jump.
      * @param board The board to work with - assumed to be flipped to correspond to this piece's color.
      */
-    public Move[] getAllPossibleMoves(Board board)
-    {
-        // create expandable list of all moves
-        ArrayList<Move> moves = new ArrayList<Move>();
+    // public Move[] getAllPossibleMoves(Board board)
+    // {
+    //     // create expandable list of all moves
+    //     ArrayList<Move> moves = new ArrayList<Move>();
         
-        // change y endpoints based on kingness and color=direction of movement
-        int startingY, yIncrement;
-        if (isWhite)
-        {
-            // if it's white, we move from further down the board backwards to possible king position
-            startingY = this.y + 1; 
-            yIncrement = -2;
-        }
-        else 
-        {
-            // if it's black, we move from further up the board forward to possible king position
-            startingY = this.y - 1;
-            yIncrement = 2;
-        }
+    //     // change y endpoints based on kingness and color=direction of movement
+    //     int startingY, yIncrement;
+    //     if (isWhite)
+    //     {
+    //         // if it's white, we move from further down the board backwards to possible king position
+    //         startingY = this.y + 1; 
+    //         yIncrement = -2;
+    //     }
+    //     else 
+    //     {
+    //         // if it's black, we move from further up the board forward to possible king position
+    //         startingY = this.y - 1;
+    //         yIncrement = 2;
+    //     }
         
-        // use kingess to determine number of rows to check
-        int rowsToCheck = 1; // default as non-king
-        if (this.isKing)
-            rowsToCheck = 2;
+    //     // use kingess to determine number of rows to check
+    //     int rowsToCheck = 1; // default as non-king
+    //     if (this.isKing)
+    //         rowsToCheck = 2;
         
-        // iterate over the four spaces where normal (non-jumping) moves are possible        
-        for (int x = this.x - 1; x <= this.x + 1; x += 2)
-        {
-            // go over the rows (or row) (we iterate the number of times determined by the kingess above)
-            int y = startingY - yIncrement; // add this so we can add the normal increment before the boundary checks
-            for (int i = 0; i < rowsToCheck; i++) 
-            {
-                // increment y if we need to (this will have no effect if we only run one iteration)
-                y += yIncrement;
+    //     // iterate over the four spaces where normal (non-jumping) moves are possible        
+    //     for (int x = this.x - 1; x <= this.x + 1; x += 2)
+    //     {
+    //         // go over the rows (or row) (we iterate the number of times determined by the kingess above)
+    //         int y = startingY - yIncrement; // add this so we can add the normal increment before the boundary checks
+    //         for (int i = 0; i < rowsToCheck; i++) 
+    //         {
+    //             // increment y if we need to (this will have no effect if we only run one iteration)
+    //             y += yIncrement;
                 
-                // check for going off end of board, in which case just skip this iteration (we may do this twice if at a corner)
-                if (board.isOverEdge(x, y))
-                    continue;
+    //             // check for going off end of board, in which case just skip this iteration (we may do this twice if at a corner)
+    //             if (board.isOverEdge(x, y))
+    //                 continue;
                 
-                // add a move here if there's not a piece 
-                if (board.getValueAt(x, y) == null)
-                {
-                    // this is not jump move in any case, and is always the first move
-                    moves.add(new Move(this.x, this.y, x, y, null, false)); 
-                }
-            }
-        }
+    //             // add a move here if there's not a piece 
+    //             if (board.getValueAt(x, y) == null)
+    //             {
+    //                 // this is not jump move in any case, and is always the first move
+    //                 moves.add(new Move(this.x, this.y, x, y, null, false)); 
+    //             }
+    //         }
+    //     }
         
-        // after we've checked all normal moves, look for and add all possible jumps (recusively as well - I mean ALL jumps)
-        Move[] possibleJumps = this.getAllPossibleJumps(board, null);
-        if (possibleJumps != null)
-            moves.addAll(Arrays.asList(possibleJumps));
+    //     // after we've checked all normal moves, look for and add all possible jumps (recusively as well - I mean ALL jumps)
+    //     Move[] possibleJumps = this.getAllPossibleJumps(board, null);
+    //     if (possibleJumps != null)
+    //         moves.addAll(Arrays.asList(possibleJumps));
 
-        // IF there are some moves, shorten and return ArrayList as a normal array
-        if (!moves.isEmpty())
-        {
-            moves.trimToSize();
-            return moves.toArray(new Move[1]); // convert to Move objects
-        }
-        else 
-            return null; // return null otherwise to symbolize no moves
-    }
+    //     // IF there are some moves, shorten and return ArrayList as a normal array
+    //     if (!moves.isEmpty())
+    //     {
+    //         moves.trimToSize();
+    //         return moves.toArray(new Move[1]); // convert to Move objects
+    //     }
+    //     else 
+    //         return null; // return null otherwise to symbolize no moves
+    // }
     
-    /**
-     * Finds all jumping moves originating from this piece.
-     * Does this recursivly; for each move a new imaginary piece will be generated,
-     * and this function will then be called on that piece to find all possible subsequent moves.
-     * @param board The board to work with - assumed to be flipped to correspond to this piece's color.
-     * @param precedingMove The moves preceding the call to search for moves off this piece - only used
-     * in recursion, should be set to null at first call. (if it's not, it means this piece is imaginary).
-     */
-    private Move[] getAllPossibleJumps(Board board, Move precedingMove)
-    {
-        // create expandable list of all moves
-        ArrayList<Move> moves = new ArrayList<Move>();
+    // /**
+    //  * Finds all jumping moves originating from this piece.
+    //  * Does this recursivly; for each move a new imaginary piece will be generated,
+    //  * and this function will then be called on that piece to find all possible subsequent moves.
+    //  * @param board The board to work with - assumed to be flipped to correspond to this piece's color.
+    //  * @param precedingMove The moves preceding the call to search for moves off this piece - only used
+    //  * in recursion, should be set to null at first call. (if it's not, it means this piece is imaginary).
+    //  */
+    // private Move[] getAllPossibleJumps(Board board, Move precedingMove)
+    // {
+    //     // create expandable list of all moves
+    //     ArrayList<Move> moves = new ArrayList<Move>();
         
-        // this is the same as above except we're doing a large cube (4x4)
-        // change y endpoints based on kingness and color=direction of movement
-        int startingY, yIncrement;
-        if (isWhite)
-        {
-            // if it's white, we move from further down the board backwards to possible king position
-            startingY = this.y + 2;
-            yIncrement = -4;
-        }
-        else 
-        {
-            // if it's black, we move from further up the board forward to possible king position
-            startingY = this.y - 2;
-            yIncrement = 4;
-        }
+    //     // this is the same as above except we're doing a large cube (4x4)
+    //     // change y endpoints based on kingness and color=direction of movement
+    //     int startingY, yIncrement;
+    //     if (isWhite)
+    //     {
+    //         // if it's white, we move from further down the board backwards to possible king position
+    //         startingY = this.y + 2;
+    //         yIncrement = -4;
+    //     }
+    //     else 
+    //     {
+    //         // if it's black, we move from further up the board forward to possible king position
+    //         startingY = this.y - 2;
+    //         yIncrement = 4;
+    //     }
         
-        // use kingess to determine number of rows to check
-        int rowsToCheck = 1; // default as non-king
-        if (this.isKing)
-            rowsToCheck = 2;
+    //     // use kingess to determine number of rows to check
+    //     int rowsToCheck = 1; // default as non-king
+    //     if (this.isKing)
+    //         rowsToCheck = 2;
         
-        // iterate over the four spaces where normal (non-jumping) moves are possible        
-        for (int x = this.x - 2; x <= this.x + 2; x += 4)
-        {
-            // go over the rows (or row) (we iterate the number of times determined by the kingess above)
-            int y = startingY - yIncrement; // add this so we can add the normal increment before the boundary checks in the loop
-            for (int i = 0; i < rowsToCheck; i++) 
-            {
-                // increment y if we need to (this will have no effect if we only run one iteration)
-                y += yIncrement;
+    //     // iterate over the four spaces where normal (non-jumping) moves are possible        
+    //     for (int x = this.x - 2; x <= this.x + 2; x += 4)
+    //     {
+    //         // go over the rows (or row) (we iterate the number of times determined by the kingess above)
+    //         int y = startingY - yIncrement; // add this so we can add the normal increment before the boundary checks in the loop
+    //         for (int i = 0; i < rowsToCheck; i++) 
+    //         {
+    //             // increment y if we need to (this will have no effect if we only run one iteration)
+    //             y += yIncrement;
                 
-                // check for going off end of board, in which case just skip this iteration (we may do this twice if at a corner)
-                if (board.isOverEdge(x, y))
-                    continue;
+    //             // check for going off end of board, in which case just skip this iteration (we may do this twice if at a corner)
+    //             if (board.isOverEdge(x, y))
+    //                 continue;
                 
-                // don't try to go backward to our old move start so we don't get in infinite recursion loops
-                if (precedingMove != null &&
-                    x == precedingMove.getStartingPosition()[0] && 
-                    y == precedingMove.getStartingPosition()[1])
-                    continue;
+    //             // don't try to go backward to our old move start so we don't get in infinite recursion loops
+    //             if (precedingMove != null &&
+    //                 x == precedingMove.getStartingPosition()[0] && 
+    //                 y == precedingMove.getStartingPosition()[1])
+    //                 continue;
                 
-                // test if there is a different-colored piece between us (at the average of our position) and the starting point 
-                // AND that there's no piece in the planned landing space (meaning we can possible jump there)
-                Piece betweenPiece = board.getValueAt( (this.x + x)/2 , (this.y + y)/2 );
-                if (betweenPiece != null &&
-                    betweenPiece.isWhite != this.isWhite &&
-                    board.getValueAt(x, y) == null)
-                {
-                    // in which case, add a move here, and note that it is a jump (we may be following some other jumps)
-                    Move jumpingMove = new Move(this.x, this.y, x, y, precedingMove, true); // origin points are absolute origin (ORIGINAL piece)
+    //             // test if there is a different-colored piece between us (at the average of our position) and the starting point 
+    //             // AND that there's no piece in the planned landing space (meaning we can possible jump there)
+    //             Piece betweenPiece = board.getValueAt( (this.x + x)/2 , (this.y + y)/2 );
+    //             if (betweenPiece != null &&
+    //                 betweenPiece.isWhite != this.isWhite &&
+    //                 board.getValueAt(x, y) == null)
+    //             {
+    //                 // in which case, add a move here, and note that it is a jump (we may be following some other jumps)
+    //                 Move jumpingMove = new Move(this.x, this.y, x, y, precedingMove, true); // origin points are absolute origin (ORIGINAL piece)
                     
-                    // then add it to our list
-                    moves.add(jumpingMove);
+    //                 // then add it to our list
+    //                 moves.add(jumpingMove);
                       
-                    // after jumping, create an imaginary piece as if it was there to look for more jumps
-                    Piece imaginaryPiece = new Piece(x, y, this.isWhite);
+    //                 // after jumping, create an imaginary piece as if it was there to look for more jumps
+    //                 Piece imaginaryPiece = new Piece(x, y, this.isWhite);
                     
-					// correspond possible jumps to this piece's kingness
-               		if (this.isKing) imaginaryPiece.setKing();
+	// 				// correspond possible jumps to this piece's kingness
+    //            		if (this.isKing) imaginaryPiece.setKing();
                     
-                    // find possible subsequent moves recusivly
-                    Move[] subsequentMoves = imaginaryPiece.getAllPossibleJumps(board, jumpingMove);
+    //                 // find possible subsequent moves recusivly
+    //                 Move[] subsequentMoves = imaginaryPiece.getAllPossibleJumps(board, jumpingMove);
                     
-                    // add these moves to our list if they exist, otherwise just move on to other possibilities
-                    if (subsequentMoves != null)
-                        moves.addAll(Arrays.asList(subsequentMoves));
-                }
-            }
-        }
+    //                 // add these moves to our list if they exist, otherwise just move on to other possibilities
+    //                 if (subsequentMoves != null)
+    //                     moves.addAll(Arrays.asList(subsequentMoves));
+    //             }
+    //         }
+    //     }
 
-        // IF there are some moves, shorten and return ArrayList as a normal array
-        if (!moves.isEmpty())
-        {
-            moves.trimToSize();
-            return moves.toArray(new Move[1]); // convert to Move arrays
-        }
-        else 
-            return null; // return null otherwise to symbolize no moves
-    }
+    //     // IF there are some moves, shorten and return ArrayList as a normal array
+    //     if (!moves.isEmpty())
+    //     {
+    //         moves.trimToSize();
+    //         return moves.toArray(new Move[1]); // convert to Move arrays
+    //     }
+    //     else 
+    //         return null; // return null otherwise to symbolize no moves
+    // }
 }
